aaf07dfb529f754f15d31a7508e3311164fa51f3
PatrickArnaud <patrick.arnaud@epitech.eu>
2024-03-15 10:13:06 +0100

    fix fixture
    password hashed

--- src/fixtures/fixtures.service.ts
+++ src/fixtures/fixtures.service.ts
@@ -2,32 +2,38 @@ import {Injectable} from "@nestjs/common";
 import {InjectModel} from "@nestjs/mongoose";
 import { Model } from 'mongoose';
 import {User, UserDocument} from "../user/entities/user.entity";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class FixturesService {
     constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
+
+    hashData(data: string) {
+        return bcrypt.hash(data, 10);
+    }
     async generateFixtures(): Promise<void> {
+        const passwordhash = await this.hashData("123")
         const users = [
             {
                 username: 'Admin',
                 email: 'Admin@fixture.com',
-                password: '123',
+                password: passwordhash,
                 role: '65040ec528f750d4de3bcdd5' ,
                 refreshToken: 'null',
             },
             {
                 username: 'Manager',
                 email: 'Manager@fixture.com',
-                password: '123',
+                password: passwordhash,
                 role:'65040ec528f750d4de3bcdd6' ,
                 refreshToken: 'null',
             },
             {
                 username: 'User',
                 email: 'User@fixture.com',
-                password: '123',
-                role: '65040ec528f750d4de3bcdd5',
+                password: passwordhash,
+                role: '65040ec528f750d4de3bcdd7',
                 refreshToken: 'null',
             },
         ];
